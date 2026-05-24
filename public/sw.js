c39bed496015c93bde97fa7c69ff700ef75ad554
Gregoire Faurobert <gregoire@faurobert.fr>
2026-05-24 10:05:25 +0200

    again

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'over-the-hill-v1779435359460'; // Bump version to force update
+const CACHE_NAME = 'over-the-hill-v1779609894848'; // Bump version to force update
 const urlsToCache = [
   '/manifest.json',
   '/favicon.ico',
